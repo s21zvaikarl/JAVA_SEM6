934a4f4503d56db5207c72c222abd01b25a8080d
s21zvaikarl <karliszvaigzne@gmail.com>
2023-05-18 14:03:08 +0300

    Added professor

---
+++ src/main/java/lv/venta/models/Professor.java
@@ -0,0 +1,61 @@
+package lv.venta.models;
+
+
+
+import org.springframework.data.annotation.Id;
+
+import com.fasterxml.jackson.core.sym.Name;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "professor_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Professor {
+	
+	@Column(name = "Idp")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idp;
+	
+	@Column(name = "Name")
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String name;
+	
+	@Column(name = "Surname")
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String surname;
+	
+	@Column(name = "Degree")
+	@NotNull
+	private Degree degree;
+	
+	public Professor(String name, String surname, Degree degree) {
+		this.name = name;
+		this.surname = surname;
+		this.degree = degree;
+	}
+	
+	
+	
+}
