f80eda397529f8b90b649478da2e4eb52b9f4492
s21zvaikarl <karliszvaigzne@gmail.com>
2023-05-18 14:19:43 +0300

    Added Grade

--- src/main/java/lv/venta/models/Grade.java
+++ src/main/java/lv/venta/models/Grade.java
@@ -1,5 +1,37 @@
 package lv.venta.models;
 
-public class Grade {
+import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "grade_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Grade {
+	@Column(name = "Idg")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idg;
+	
+	@Column(name = "GValue")
+	@Min(0)
+	@Max(10)
+	private int gvalue;
+	
+	
 }
